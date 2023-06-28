251dd03f9c2e3072eb6e142544f55dd5b814c4a1
Alex Johnson <johnsonav1992@gmail.com>
2023-06-27 20:57:59 -0500

    clg

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,11 @@
 const getKeys = <TObj extends {}>(obj: TObj) => {
     return Object.keys(obj)
 }
+
+const user = {
+    name: 'Alex'
+    , age: 31
+    , occupation: 'Software Engineer'
+}
+
+console.log(getKeys(user))
