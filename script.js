ca5deaf822da21fc3ccda9d0a6e94c53adbe1aa8
Alex Johnson <johnsonav1992@gmail.com>
2023-06-28 20:28:21 -0500

    problem 2

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const person = {
     firstName: 'Alex',
     lastName: 'Johnson'
 };
+// Example usage
+const person2 = {
+    firstName: 'Eliot',
+    lastName: 'Johnson'
+};
+// person2.firstName = 'John'  ------ (Err: Cannot assign to firstName because it is a read-only property.)
--- script.ts
+++ script.ts
@@ -1,11 +1,14 @@
 ///// TYPESCRIPT CODING CHALLENGES /////
 
+////////////////////////////////////////////////////////////////////
 /* 1. Implement the built-in Pick<T, K> generic without using it.
     Constructs a type by picking the set of properties K from T 
 */
+////////////////////////////////////////////////////////////////////
 
 type MyPick<T, K extends keyof T> = { [SelectedKeys in K]: T[SelectedKeys] }
 
+// Example usage
 interface Person { 
     firstName: string
     , lastName: string 
@@ -17,3 +20,22 @@ const person: Person = {
 }
 
 type FirstNameOnly = MyPick<Person, 'firstName'>
+
+
+////////////////////////////////////////////////////////////////////
+/* 2. Implement the built-in Readonly<T> generic without using it.
+    Constructs a type with all properties of T set to readonly, meaning 
+    the properties of the constructed type cannot be reassigned.
+*/
+////////////////////////////////////////////////////////////////////
+
+type MyReadonly<Obj> = { readonly [Property in keyof Obj]: Obj[Property] }
+
+// Example usage
+
+const person2: MyReadonly<Person> = {
+    firstName: 'Eliot'
+    , lastName: 'Johnson'
+}
+
+// person2.firstName = 'John'  ------ (Err: Cannot assign to firstName because it is a read-only property.)
